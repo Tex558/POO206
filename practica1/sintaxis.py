3530ac31cc6c259a98b11016b9f0abf6ef22eba5
Tex558 <122042676@upq.edu.mx>
2025-05-16 18:19:03 -0600

    Variables en python

--- practica1/sintaxis.py
+++ practica1/sintaxis.py
@@ -9,5 +9,27 @@ en python """
 
 
 #2. strings
-print("hola soy una cadena")
 print('yo soy otra')
+
+variable1= "hola soy una cadena"
+
+print(len(variable1)) #len = length (cuenta la longitu de la candena)
+print (variable1[2:5]) #print por partes (del 2ndo al 5nto)
+print (variable1[2:])
+print (variable1[:5])
+
+#3. variables
+
+x= "Emiliano Jimenez"
+x= 4
+x= 5.76
+print (x) #toma el ultimo valor
+
+x= int(3) #guardar un TIPO de dato especifico
+y= float (3)
+z= str (3)
+
+print(x,y,z)
+print(type (x)) #type = ultimo tipo de la cadena
+print(type (y))
+print(type (z))
