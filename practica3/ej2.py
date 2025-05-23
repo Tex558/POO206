b1a4a3393a57ad0301a18255a14b732a93a12df2
Tex558 <122042676@upq.edu.mx>
2025-05-22 20:17:12 -0600

    ej corregido

--- practica3/ej2.py
+++ practica3/ej2.py
@@ -1,12 +1,14 @@
 while True:
+    salida = input("Introduce un año: ")
+    if salida.lower() == 'p':
+        break
     try:
-        año = int(input("Introduce un año: "))
-
+        año = int(salida)
+        if año < 0:
+            raise ValueError
         if año % 4 == 0:
             print("El año es biciesto")
         else:
             print("El año no es biciesto")
-
     except ValueError:
-        print("Error: Se ingreso algo que no es un numero entero")
-        break
+        print("Error: Se ingreso algo que no es un año")
