e49457e15ea3d93703bf57e7b80327477c73a0d9
Tex558 <122042676@upq.edu.mx>
2025-05-22 20:12:42 -0600

    ej1 correjido

--- practica3/ej1.py
+++ practica3/ej1.py
@@ -1,12 +1,14 @@
 while True:
+    salida = input("Introduce un numero: ")
+    if salida.lower() == 'p':
+        break
     try:
-        numero = int(input("Introduce un numero: "))
-
+        numero = int(salida)
+        if numero < 0:
+            raise ValueError
         if numero % 2 == 0:
             print("El numero es par")
         else:
             print("El numero no es par")
-
     except ValueError:
         print("Error: Se ingreso algo que no es un numero entero")
-        break
--- practica3/ej2.py
+++ practica3/ej2.py
@@ -8,5 +8,5 @@ while True:
             print("El año no es biciesto")
 
     except ValueError:
-        print("Error: Se ingreso algo que no es un año")
+        print("Error: Se ingreso algo que no es un numero entero")
         break
