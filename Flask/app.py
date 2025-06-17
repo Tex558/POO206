1fb247869cd3f3621db511a591cc1dc8ac6adf07
Tex558 <122042676@upq.edu.mx>
2025-06-17 16:36:42 -0600

    conexion BD

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,26 @@
 
-from flask import Flask
+from flask import Flask, jsonify
+from flask_mysqldb import MySQL
+import MySQLdb
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'Emiliano04'
+app.config['MYSQL_DB'] = 'dbflask'
+
+mysql = MySQL(app)
+
+@app.route('/DBCheck')
+def DB_check():
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('Select 1')
+        return jsonify( {'status':'ok','message':'Conectado con exito'} ),200
+    except MySQLdb.MySQLError as e:
+        return jsonify( {'status':'error','message':str(e)} ),500
+
 
 #Ruta simple
 @app.route('/')
