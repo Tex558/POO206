e79061494caf713aa2f626f92d6ef359f90b2b7a
Tex558 <122042676@upq.edu.mx>
2025-06-20 20:57:40 -0600

    practica 8

--- Flask/app.py
+++ Flask/app.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
 from flask_mysqldb import MySQL
 import MySQLdb
 
@@ -9,6 +9,8 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'Emiliano04'
 app.config['MYSQL_DB'] = 'dbflask'
+
+app.secret_key ='mysecretkey'
 
 mysql = MySQL(app)
 
@@ -22,31 +24,48 @@ def DB_check():
         return jsonify( {'status':'error','message':str(e)} ),500
 
 
-#Ruta simple
+#Ruta principal y de consulta
 @app.route('/')
 def home():
-    return 'Hola Mundo FLASK'
+    return render_template('formulario.html')
 
-#Ruta con parametros
-@app.route('/saludo/<nombre>')
-def saludar(nombre):
-    return 'Un saludo a la grasa '+ nombre +'!'
+@app.route('/consulta')
+def consulta():
+    return render_template('consulta.html')
+
+#Ruta insert
+@app.route('/guardarAlbum', methods=['POST'])
+def guardar():
+
+    #obtenen datos al ingresar
+    titulo = request.form.get('txtTitulo','').strip()
+    artista = request.form.get('txtArtista','').strip()
+    anio = request.form.get('txtAnio','').strip()
+
+    #Intentar ejecutar el insert
+    try:
+        cursor= mysql.connection.cursor()
+        cursor.execute('insert into albums(Titulo,Artista,Año) values(%s,%s,%s)',(titulo, artista, anio))
+        mysql.connection.commit()
+        flash('Album guardado en BD')
+        return redirect(url_for('home')) 
+    
+    except Exception as e:
+        mysql.connection.rollback()
+        flash ('Error al guardar BD: ' + str(e))
+        return redirect(url_for('home')) 
+
+    finally:
+        cursor.close()
 
 #Ruta try-catch
 @app.errorhandler(404)
 def PagNoE(e):
-    return 'EY! Aprenda a escribir Lerdo :)'
+    return 'EY! Aprenda a escribir :)'
 
-#Ruta doble
-@app.route('/usuario')
-@app.route('/usuaria')
-def dobleroute():
-    return 'Soy el mismo recurso en diferente pagina ;)'
-
-#Ruta POST
-@app.route('/formulario', methods=['POST'])
-def formulario():
-    return 'Soy un fomrulario'
+@app.errorhandler(405)
+def PagNoE(e):
+    return 'EY! Revisa el metodo de envio :)'
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    app.run(port = 3000, debug = True)
